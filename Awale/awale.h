3dbfb222fc0ce12c0a25288fb37bb9a6bc266aee
Benjamin Maison <benjamin.maison@insa-lyon.fr>
2023-11-24 11:56:10 +0100

    complete awale class

--- Awale/awale.h
+++ Awale/awale.h
@@ -1,1 +1,24 @@
-typedef 
+#define NUM_HOLES 6
+
+/* Board visualisation
+    f e d c b a
+    0 1 2 3 4 5 
+    0 1 2 3 4 5
+    A B C D E F
+*/
+// 0 is the first player, 1 is the second player
+// 0 is the first row, 1 is the second row
+
+
+typedef int Board[2][NUM_HOLES];
+
+typedef struct GameState{
+    Board board;
+    int currentPlayer;
+    int score[2];
+} GameState;
+
+void init();
+GameState playTurn(int hole);
+int isLegal();
+int isFinished(); 
