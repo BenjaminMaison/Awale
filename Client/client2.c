210bcd9763add7963e72665fd56792c8db2034e4
Benjamin Maison <benjamin.maison@insa-lyon.fr>
2023-11-28 11:20:11 +0100

    add serialize function for gamestate

--- Client/client2.c
+++ Client/client2.c
@@ -248,7 +248,14 @@ static void displayPlayers(char* buffer){
     }
 }
 
-int parseGameState(const char* buffer, GameState* gameState) {
+/**
+ * @brief Deserialize a game state from a buffer
+ * 
+ * @param buffer 
+ * @param gameState 
+ * @return int - EXIT_SUCCESS if success, EXIT_FAILURE otherwise
+ */
+int deserializeGameState(const char* buffer, GameState* gameState) {
    const char * separators = "\n ";
    char* token = strtok(buffer, separators);
    if(strcmp(token, "gameState") != 0){
@@ -277,6 +284,35 @@ int parseGameState(const char* buffer, GameState* gameState) {
    gameState->score[1] = atoi(token);
 
    return EXIT_SUCCESS;  
+}
+
+/**
+ * @brief  Serialize a game state into a buffer
+ * 
+ * @param gameState 
+ * @param buffer 
+ * @return int - EXIT_SUCCESS if success, EXIT_FAILURE otherwise
+ */
+int serializeGameState(const GameState* gameState, char* buffer){
+   char temp[BUF_SIZE];
+   strcpy(buffer, "gameState\n");
+   for(int i = 0; i < NUM_HOLES; i++){
+      sprintf(temp, "%d ", gameState->board[0][i]);
+      strcat(buffer, temp);
+   }
+   strcat(buffer, "\n");
+   for(int i = 0; i < NUM_HOLES; i++){
+      sprintf(temp, "%d ", gameState->board[1][i]);
+      strcat(buffer, temp);
+   }
+   strcat(buffer, "\n");
+   sprintf(temp, "%d\n", gameState->currentPlayer);
+   strcat(buffer, temp);
+   sprintf(temp, "%d\n", gameState->score[0]);
+   strcat(buffer, temp);
+   sprintf(temp, "%d\n", gameState->score[1]);
+   strcat(buffer, temp);
+   return EXIT_SUCCESS;
 }
 
 
